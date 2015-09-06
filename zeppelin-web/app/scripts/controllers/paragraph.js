67dd43503937d5b5bda3e6be98917c77f1e0e546
SangminPark <bestmenbal@gmail.com>
2015-09-06 23:48:12 +0900

    노트북 사이드바에 데이타소스 목록 추가  - 데이타 목록 drag 추가  - paragraph editor 에 데이타 소스 drop 추가

--- zeppelin-web/app/scripts/controllers/paragraph.js
+++ zeppelin-web/app/scripts/controllers/paragraph.js
@@ -2040,7 +2040,21 @@ console.info('data', data);
     }
   };
 
-
+  $scope.$on('onDragStart_datasourceFromSidebar', function(event, dragDatasource) {
+  	$scope.dragDatasource = dragDatasource;
+  });
+  
+  $scope.$on('onDragging_datasourceFromSidebar', function(event, position) {
+  	var cursor = $scope.editor.renderer.screenToTextCoordinates(position.pageX, position.pageY);
+  	$scope.editor.moveCursorToPosition(cursor)
+  });
+    
+  $scope.onDroppedDatasoruce = function(event, ui) {
+  	var position = $scope.editor.getCursorPosition();
+  	$scope.editor.getSession().insert(position, "\"" + $scope.dragDatasource.datsrcName + "\"");
+  	$scope.$parent.toggleSidebar();
+  };
+  
   $scope.onGraphOptionChange = function() {
     clearUnknownColsFromGraphOption();
     $scope.setGraphMode($scope.paragraph.config.graph.mode, true, false);

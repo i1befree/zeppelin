404ee7dce8bd071efd9eec29dfcf879d06646f67
i1befree <i1befree@gmail.com>
2015-09-09 11:07:05 +0900

    datastorerepository 에 Repository 어노테이션 추가(테스트용).

--- zeppelin-server/src/main/java/com/sktelecom/cep/repository/DataStoreRepository.java
+++ zeppelin-server/src/main/java/com/sktelecom/cep/repository/DataStoreRepository.java
@@ -2,11 +2,14 @@ package com.sktelecom.cep.repository;
 
 import com.sktelecom.cep.entity.DataStore;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
- *
+ * DataStoreRepository
  */
+@Repository("dataStoreRepository")
 public interface DataStoreRepository extends JpaRepository<DataStore, String> {
-  Page<DataStore> findByName(String name);
+  Page<DataStore> findByNameOrderByUpdateTimeDesc(String name, Pageable pageable);
 }

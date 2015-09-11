91c469dd041850f4b6a1dd965e5e466f95c1a120
i1befree <i1befree@gmail.com>
2015-09-11 09:36:38 +0900

    save, findone 용 테스트 템플릿 추가.

--- zeppelin-server/src/test/java/com/sktelecom/cep/repository/DataStoreRepositoryTest.java
+++ zeppelin-server/src/test/java/com/sktelecom/cep/repository/DataStoreRepositoryTest.java
@@ -1,29 +1,59 @@
 package com.sktelecom.cep.repository;
 
 import com.sktelecom.cep.entity.DataStore;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import com.sktelecom.cep.entity.DataStore.SubType;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.sql.Timestamp;
+
+import static com.sktelecom.cep.entity.DataStore.Type;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DataStoreRepositoryTest {
-  GenericXmlApplicationContext ctx;
+  static GenericXmlApplicationContext ctx;
 
-  @Before
-  public void setUp(){
+  @BeforeClass
+  public static void setUp(){
     ctx = new GenericXmlApplicationContext();
     ctx.load("classpath:repository-test-context.xml");
     ctx.refresh();
   }
 
+  @Test
   public void testSave(){
     DataStoreRepository repository = ctx.getBean("dataStoreRepository", DataStoreRepository.class);
+    DataStore dataStore = new DataStore();
+    dataStore.setName("database test");
+    dataStore.setDescription("DataStore test with mysql setting");
+    dataStore.setHostName("cep1");
+    dataStore.setPassword("mytest");
+    dataStore.setPortNum(3306);
+    dataStore.setSubType(SubType.MSSQL);
+    dataStore.setType(Type.DATABASE);
+    dataStore.setUpdateTime(new Timestamp(System.currentTimeMillis()));
+    dataStore.setUpdateUserId("admin");
+    dataStore.setUsername("admin");
+
+    DataStore returnVal = repository.save(dataStore);
+
+    assertNotNull(returnVal.getId());
+    assertEquals("database test", returnVal.getName());
+  }
+
+  @Test
+  public void testFindOne(){
+    DataStoreRepository repository = ctx.getBean("dataStoreRepository", DataStoreRepository.class);
+    DataStore dataStore = repository.findOne("5c9439ee-ca70-4878-9e38-0ca6d3bd6eea");
+
+    assertNotNull(dataStore);
+    assertEquals("mymeta", dataStore.getName());
   }
 
   @Test
@@ -32,6 +62,6 @@ public class DataStoreRepositoryTest {
     Pageable pageable = new PageRequest(0, 10);
     Page<DataStore> dataStores = repository.findByNameLikeOrderByUpdateTimeDesc("%mymeta%", pageable);
 
-    Assert.assertEquals(1, dataStores.getTotalPages());
+    assertEquals(1, dataStores.getTotalPages());
   }
 }
